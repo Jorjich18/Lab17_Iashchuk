c9e472d78c211b4410a1e5663330b279d51f8361
Georgiy <makoson355@gmail.com>
2024-12-10 13:21:17 +0200

    Exersize_2

---
+++ exersize2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void change(double a[], int n, double& min)
+{
+	int i;
+	min = a[0];
+	for (i = 0; i < n; i++)
+		if (a[i] < min) min = a[i];
+	for (i = 0; i < n; i++)
+		if (a[i] == 0) a[i] = min;
+}
+
+int main()
+{
+	double a[9], min;
+	cout << "Input 9 float numbers:\n";
+	for (int i = 0; i < 9; i++) cin >> a[i];
+	change(a, 15, min);
+	cout << "Changed array:\n";
+	for (int i = 0; i < 15; i++) cout << a[i] << "\t";
+	cout << endl;
+	return 0;
+}
